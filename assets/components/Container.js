f837ccaceff13011bdaaf28e68b6b10a133d532a
johnnyjwaity <johnnywaity@gmail.com>
2019-09-09 00:29:32 -0500

    Finished Featured projects

--- assets/components/Container.js
+++ assets/components/Container.js
@@ -3,12 +3,19 @@ class Container extends React.Component {
         super(props)
     }
     render() {
+        let elements = []
+        for (let i = 0; i < this.props.data.length; i++) {
+            let d = this.props.data[i]
+            d.key = i
+            elements.push(e(this.props.child, d, null))
+        }
         let c = e("div", {
             key: 1, style: {
                 display: "flex",
-                justifyContent: "space-around"
+                justifyContent: "space-around",
+                flexWrap: "wrap"
             }
-        }, e(this.props.child, { name: "Flux" }, null))
+        }, elements)
         return e('article', { className: 'sec-container' }, [e('h2', { key: 0 }, this.props.name), c])
     }
 }
--- assets/components/FeaturedProject.js
+++ assets/components/FeaturedProject.js
@@ -5,17 +5,17 @@ class FeaturedProject extends React.Component {
             hover: false
         }
         this.hover = this.hover.bind(this)
-        
+
     }
-    hover(){
+    hover() {
         this.setState({
             hover: !this.state.hover
         })
     }
     render() {
         var imageContainerStyle = {
-            backgroundImage: "linear-gradient(rgb(68, 153, 243), rgb(112, 241, 135))",
-            width: "18em",
+            backgroundImage: this.props.background,
+            width: "20em",
             height: "12em",
             display: "flex",
             justifyContent: "space-around",
@@ -23,16 +23,16 @@ class FeaturedProject extends React.Component {
             borderRadius: "10px",
             transition: "transform 0.8s ease-in-out"
         }
-        if(this.state.hover){
+        if (this.state.hover) {
             imageContainerStyle.transform = "translateY(-10px)"
-        }else{
+        } else {
 
         }
         var imageContainer = e("div", {
             style: imageContainerStyle,
             key: 0
         }, e("img", {
-            src: "assets/images/flux.png",
+            src: this.props.icon,
             style: {
                 borderRadius: "22%",
                 width: "40%",
@@ -41,38 +41,69 @@ class FeaturedProject extends React.Component {
         }))
         let title = e("h4", {
             style: {
-                fontSize: "3em",
+                fontSize: this.props.fontSize,
                 margin: 0,
                 marginLeft: "0em",
-                color: '#56bcc8',
-                fontFamily: "Amandita, San Francisco",
+                color: this.props.fontColor ? this.props.fontColor : '#274690',
+                fontFamily: this.props.font ? this.props.font : "Roboto Slab",
                 textAlign: "center"
             },
             key: 0
-        }, "Flux")
+        }, this.props.name)
         let description = e("p", {
             style: {
                 margin: 0,
+                marginBottom: "5px",
                 color: "grey",
                 textAlign: "center"
             },
             key: 1
-        }, "Surveys Made Easy")
+        }, this.props.description)
 
-        let productPageButton = e("button",{
+        let productPageButton = e("button", {
             style: {
-
+                marginBottom: "5px",
+                display: this.props.productLink ? "inline-block" : "none",
+                textDecoration: "none",
+                border: "none",
+                backgroundColor: "#274690",
+                color: "white",
+                // width: "90%",
+                // height: "40px",
+                padding: "8px 10px",
+                fontSize: "20px",
+                borderRadius: "8px"
             },
+            onClick: () => { window.open(this.props.productLink) },
             key: 2
         }, "Visit Product Page")
         let appStoreButton = e("input", {
             type: "image",
-            src: "assets/images/appstore.png"
+            src: "assets/images/appstore.png",
+            key: 0,
+            style: {
+                display: this.props.appStoreLink ? "inline-block" : "none"
+            },
+            onClick: () => { window.open(this.props.appStoreLink) }
         })
         let googlePlayButton = e("input", {
             type: "image",
-            src: "assets/images/googleplay.png"
+            src: "assets/images/googleplay.png",
+            key: 1,
+            style: {
+                display: this.props.googlePlayLink ? "inline-block" : "none"
+            },
+            onClick: () => { window.open(this.props.googlePlayLink) }
         })
+        let storeContainer = e("div", {
+            style: {
+                display: "flex",
+                justifyContent: "space-around",
+                width: "100%",
+                marginBottom: "5px"
+            },
+            key: 3
+        }, [appStoreButton, googlePlayButton])
         var textContainerStyle = {
             backgroundColor: "white",
             width: "90%",
@@ -83,15 +114,15 @@ class FeaturedProject extends React.Component {
             flexDirection: "column",
             alignItems: "center"
         }
-        if(this.state.hover){
+        if (this.state.hover) {
 
-        }else{
+        } else {
             textContainerStyle.transform = "translateY(-10px)"
         }
         var textContainer = e("div", {
             key: 1,
             style: textContainerStyle
-        }, [title, description, productPageButton, appStoreButton, googlePlayButton])
+        }, [title, description, productPageButton, storeContainer])
         let container = e("div", {
             style: {
                 padding: "5px",
